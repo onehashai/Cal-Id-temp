20751d648ab4ea2f939781a3db83b3b1874f150b
avnotaklu <sukhmander111101@gmail.com>
2025-08-10 18:06:17 +0530

    Routing section all sections except embeddings done

--- packages/calid/ui/components/switch.tsx
+++ packages/calid/ui/components/switch.tsx
@@ -1,25 +1,25 @@
 import { cn } from "@calid/features/lib/cn";
-import * as SwitchPrimitives from "@radix-ui/react-switch";
+import * as PrimitiveSwitch from "@radix-ui/react-switch";
 import * as React from "react";
 
 const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+  React.ElementRef<typeof PrimitiveSwitch.Root>,
+  React.ComponentPropsWithoutRef<typeof PrimitiveSwitch.Root>
 >(({ className, ...props }, ref) => (
-  <SwitchPrimitives.Root
+  <PrimitiveSwitch.Root
     className={cn(
-      "focus-visible:ring-ring focus-visible:ring-offset-background data-[state=checked]:bg-primary data-[state=unchecked]:bg-input peer inline-flex h-5 w-10 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "focus-visible:ring-ring focus-visible:ring-offset-background data-[state=unchecked]:bg-default data-[state=checked]:bg-cal-active peer inline-flex h-5 w-10 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
       className
     )}
     {...props}
     ref={ref}>
-    <SwitchPrimitives.Thumb
+    <PrimitiveSwitch.Thumb
       className={cn(
-        "bg-background pointer-events-none block h-4 w-4 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
+        "bg-default pointer-events-none block h-4 w-4 rounded-full shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"
       )}
     />
-  </SwitchPrimitives.Root>
+  </PrimitiveSwitch.Root>
 ));
-Switch.displayName = SwitchPrimitives.Root.displayName;
+Switch.displayName = PrimitiveSwitch.Root.displayName;
 
 export { Switch };
